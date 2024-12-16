c65b4aa7c4668db30df796582ba1fafbbda3737e
Андроид243 <android243@zamt.local>
2024-12-16 13:25:31 +0300

    Добавление перехода между экранами

--- app/src/main/java/com/example/smartlab/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/MainActivity.kt
@@ -1,50 +1,79 @@
 package com.example.smartlab
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.smartlab.layouts.OnBoard
 import com.example.smartlab.ui.theme.SmartLabTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         installSplashScreen()
         setContent {
+            val pagerState = rememberPagerState(pageCount = {
+                3
+            })
             SmartLabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HorizontalPager(state = pagerState) {page->
+                        if (page == 0) {
+
+                            OnBoard(
+                                buttomText = "Пропустить", headeText = "Анализы",
+                                descriptionText = "Экспресс сбор и получение проб",
+                                dotsImageVector = ImageVector.vectorResource(R.drawable.group_1),
+                                illustration = ImageBitmap.imageResource(R.drawable.photo_1),
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }else if (page==1) {
+
+                            OnBoard(
+                                buttomText = "Пропустить", headeText = "Уведомления",
+                                descriptionText = "Вы быстро узнаете о результатах",
+                                dotsImageVector = ImageVector.vectorResource(R.drawable.group_2),
+                                illustration = ImageBitmap.imageResource(R.drawable.photo_2),
+                                modifier = Modifier.padding(innerPadding))
+                        }else{
+
+                            OnBoard(
+                                buttomText = "Завершить", headeText = "Мониторинг",
+                                descriptionText = "Наши врачи всегда наблюдают \n" +
+                                        "за вашими показателями здоровья",
+                                dotsImageVector = ImageVector.vectorResource(R.drawable.group_3),
+                                illustration = ImageBitmap.imageResource(R.drawable.photo_3),
+                                modifier = Modifier.padding(innerPadding))
+                        }
+
+
+
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartLabTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/smartlab/layouts/OnBoard.kt
@@ -0,0 +1,58 @@
+package com.example.smartlab.layouts
+
+import android.media.audiofx.AudioEffect.Descriptor
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.controls.ui.theme.Components.OnboardDescription
+import com.example.controls.ui.theme.Components.OnboardHeader
+import com.example.controls.ui.theme.Components.TextButton
+import com.example.smartlab.R
+
+import org.w3c.dom.Text
+
+@Composable
+fun OnBoard(modifier: Modifier = Modifier,buttomText: String,headeText: String,descriptionText: String, dotsImageVector: ImageVector,illustration: ImageBitmap) {
+    Column(modifier = modifier.fillMaxSize().padding(20.dp)) {
+
+        Row {
+            TextButton(text = buttomText, modifier = Modifier.weight(1f), onClick = {})
+            Image(ImageVector.vectorResource(R.drawable.shape),null, modifier = Modifier.weight(1f))
+        }
+        Spacer(Modifier.height(29.dp))
+        OnboardHeader(text = headeText, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(Modifier.height(29.dp))
+        OnboardDescription(text = descriptionText, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(Modifier.height(60.dp))
+        Image(dotsImageVector,null, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(Modifier.weight(1f))
+        Image(illustration,null, modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxSize(0.8f))
+    }
+}
+
+@Preview
+@Composable
+private fun OnBoardPrewiew() {
+    OnBoard(
+        buttomText = "Пропустить", headeText = "Анализы",
+        descriptionText = "Экспресс сбор и получение проб",
+        dotsImageVector = ImageVector.vectorResource(R.drawable.group_1),
+        illustration = ImageBitmap.imageResource(R.drawable.photo_1))
+}
